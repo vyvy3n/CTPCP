37a976da05ebbd8f3fb8b593669f8aa834ac9462
ThitherShore <thither_shore@163.com>
2017-09-16 04:26:26 -0400

    update

--- solveTCPCP.m
+++ solveTCPCP.m
@@ -3,11 +3,15 @@ clear
 % read image
 I = double(imread('./image/testimg.jpg'));
 X = I/255;
-X = X(81:120,1:40,:);
+X = X(41:60,41:60,:);
 dim = size(X);
 % Noise on X
+Xn = X;
 rng(0)
-X = X+rand(size(X)).*(rand(size(X))<0.3);
+ind = find(rand(numel(X),1)<0.05);
+Xn(ind) = rand(length(ind),1);
+
+%Xn = X+rand(size(X)).*(rand(size(X))<0.3);
 
 observeNoise = 0; % 1 for "add noise", 0 for "no noise" on observations
 lambda = 1/sqrt(max(dim(1:2))*dim(3)) % lambda in "||L||_* + \lambda ||S||_1"
@@ -17,31 +21,46 @@ opts.tol = 1e-6;
 opts.rho = 1.2;
 opts.max_iter = 300;
 opts.DEBUG = 1;
+optS.penalty = 1e4;
 
 %% Sampling
 
 % q: int, the number of sampled obeservations
-q = floor(0.5*numel(X));
+q = floor(0.5*numel(Xn));
 % Gauss sampling matrix GM
-GM = randn(q,numel(X));
-g = GM*X(:); % sample
+GM = randn(q,numel(Xn));
+GM2 = GM'*GM;
+g = GM*Xn(:); % sample
 % Noise on sampling observations
 if observeNoise == 1
-    g = g+ones(q,1).*(0.01*max(abs(g))*normrnd(0,1,[q,1]));
+    %g = g+ones(q,1).*(0.01*max(abs(g))*normrnd(0,1,[q,1]));
+    ind = find(rand(numel(g),1)<0.3);
+    g(ind) = rand(length(ind),1);
 end
-
+%imshow(reshape(pinv(GM'*GM)*GM'*g,dim))
 %% slove TCPCP
 
 tic
-[L, S, obj, err, iter] = tcpcp(dim,g,GM,lambda,opts);
+[L, S, obj, err, iter] = tcpcp(dim,g,GM,GM2,lambda,opts);
 toc
 
 maxP = max(abs(X(:)));
 
 figure(1)
-subplot(1,3,1)
+subplot(2,2,1)
 imshow(X/maxP)% original picture
-subplot(1,3,2)
+title('Original Image');
+subplot(2,2,2)
+imshow(Xn/maxP)% original picture
+title('Noise Observation');
+subplot(2,2,3)
 imshow(L/maxP)% L solved by TCPCP
-subplot(1,3,3)
+title('L');
+subplot(2,2,4)
 imshow(S/maxP)% S solved by TCPCP
+title('S');
+
+savefig('TCPCPresult.fig')
+save('TCPCPresult.mat')
+
+max(abs(S(:)))
--- tcpcp.m
+++ tcpcp.m
@@ -1,4 +1,4 @@
-function [L,S,obj,err,iter] = tcpcp(dim,g,GM,lambda,opts)
+function [L,S,obj,err,iter] = tcpcp(dim,g,GM,GM2,lambda,opts)
 
 % Tensor Compressive Principal Component Pursuit Algomrith
 %
@@ -19,10 +19,10 @@ function [L,S,obj,err,iter] = tcpcp(dim,g,GM,lambda,opts)
 %           opts.max_iter   -   maximum number of iterations
 %           opts.mu         -   stepsize for dual variable updating in ADMM
 %           opts.max_mu     -   maximum stepsize
-%           opts.rho        -   rho>=1, ratio used to increase mu
-%           opts.DEBUG      -   0 or 1
+%           opts.rho       -   0 or 1
 %
-% Output:
+% Output:      -   rho>=1, ratio used to increase mu
+%           opts.DEBUG 
 %       L       -    d1*d2*d3 tensor
 %       S       -    d1*d2*d3 tensor
 %       obj     -    objective function value
@@ -35,6 +35,7 @@ rho = 1.1;
 mu = 1e-4;
 max_mu = 1e10;
 DEBUG = 0;
+penalty = 1e3;
 
 if ~exist('opts', 'var')
     opts = [];
@@ -44,6 +45,7 @@ if isfield(opts, 'max_iter');    max_iter = opts.max_iter;    end
 if isfield(opts, 'rho');         rho = opts.rho;              end
 if isfield(opts, 'mu');          mu = opts.mu;                end
 if isfield(opts, 'max_mu');      max_mu = opts.max_mu;        end
+if isfield(opts, 'penalty');     penalty = opts.penalty;      end
 if isfield(opts, 'DEBUG');       DEBUG = opts.DEBUG;          end
 
 L = zeros(dim);
@@ -59,26 +61,27 @@ for iter = 1 : max_iter
     Lk = L;
     Sk = S;
     % update P
-    [P,tnnP] = prox_tnn(L+Z1,1/mu);
+    [P,tnnP] = prox_tnn(L+Z1/mu,1/mu);
     % update Q
-    S = prox_l1(S+Z2,lambda/mu);
+    Q = prox_l1(S+Z2/mu,lambda/mu);
     % update L
-    GM2 = GM'*GM;
-    L = reshape(pinv(GM2+eye(m))*(GM'*g+P(:)-GM2*S(:)),dim);
+    L = reshape(pinv(penalty*GM2+mu*eye(m))*(penalty*GM'*g+mu*P(:)-Z1(:)-penalty*GM2*S(:)),dim);
     % update S
-    S = reshape(pinv(GM2+eye(m))*(GM'*g+Q(:)-GM2*L(:)),dim);
+    S = reshape(pinv(penalty*GM2+mu*eye(m))*(penalty*GM'*g+mu*Q(:)-Z2(:)-penalty*GM2*L(:)),dim);
     % dual update difference
     dZ1 = L-P;
     dZ2 = S-Q;
     chgL = max(abs(Lk(:)-L(:)));
     chgS = max(abs(Sk(:)-S(:)));
-    chg = max([ chgL chgS max(abs(dZ1(:))) max(abs(dZ2(:))) ]);
+    chg = max([ chgL chgS max(abs(dZ1(:))) max(abs(dZ2(:)))]);
     if DEBUG
         if iter == 1 || mod(iter, 10) == 0
             obj = tnnP+lambda*norm(S(:),1);
-            err = norm(dZ1(:))+norm(dZ2(:));
+            err = norm(dZ1(:))+norm(dZ2(:))+norm(GM*(L(:)-S(:))-g);
             disp(['iter ' num2str(iter) ', mu=' num2str(mu) ...
-                    ', obj=' num2str(obj) ', err=' num2str(err)]); 
+                    ', obj=' num2str(obj) ', err=' num2str(err)])%...
+                    %', norm(P)=' num2str(norm(abs(P(:)))) ', norm(Q)=' num2str(norm(abs(Q(:))))...
+                    %', norm(L)=' num2str(norm(abs(L(:)))) ', norm(S)=' num2str(norm(abs(S(:))))]); 
         end
     end
     
@@ -86,9 +89,9 @@ for iter = 1 : max_iter
         break;
     end 
     % dual update Z1
-    Z1 = Z1 + dZ1;
+    Z1 = Z1 + mu*dZ1;
     % dual update Z2
-    Z2 = Z2 + dZ2;
+    Z2 = Z2 + mu*dZ2;
     mu = min(rho*mu,max_mu);    
 end
 obj = tnnP+lambda*norm(S(:),1);

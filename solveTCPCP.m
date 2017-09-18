2517731af1960f3f25cc6be9df545a272a89f445
Tianwei Yue <thither_shore@163.com>
2017-09-17 21:36:22 -0400

    Add files via upload

--- solveTCPCP.m
+++ solveTCPCP.m
@@ -1,34 +1,32 @@
 addpath(genpath(cd))
 clear
-% % read image
-% I = double(imread('./image/testimg.jpg'));
-% X = I/255;
-% X = X(41:60,41:60,:);
-% dim = size(X);
-% % Noise on X
-% Xn = X;
-% rng(0)
-% ind = find(rand(numel(X),1)<0.05);
-% Xn(ind) = rand(length(ind),1);
-X  = ones(20,20,3);
-Xn = X;
+% read image
+I = double(imread('./image/testimg.jpg'));
+X = I/255;
+X = X(31:60,31:60,:);
 dim = size(X);
-Xn(15,15,:) = zeros(1,1,3);
+% Noise on X
+Xn = X;
+%rng(0)
+%ind = find(rand(numel(X),1)<0.01);
+seed = 0916;
+rand('state',seed);
+Xn = X+1.5*rand(size(X)).*(rand(size(X))<0.02);
 
 observeNoise = 0; % 1 for "add noise", 0 for "no noise" on observations
 lambda = 1/sqrt(max(dim(1:2))*dim(3)) % lambda in "||L||_* + \lambda ||S||_1"
 
-opts.mu = 1e-1;
+%opts.mu = 0.1;
 opts.tol = 1e-6;
-opts.rho = 1.05;
-opts.max_iter = 200;
+%opts.rho = 1.1;
+opts.max_iter = 500;
 opts.DEBUG = 1;
 optS.penalty = 1e4;
 
 %% Sampling
 
 % q: int, the number of sampled obeservations
-q = floor(0.9*numel(Xn));
+q = floor(0.8*numel(Xn));
 % Gauss sampling matrix GM
 GM = randn(q,numel(Xn));
 GM2 = GM'*GM;
@@ -36,7 +34,7 @@ g = GM*Xn(:); % sample
 % Noise on sampling observations
 if observeNoise == 1
     %g = g+ones(q,1).*(0.01*max(abs(g))*normrnd(0,1,[q,1]));
-    ind = find(rand(numel(g),1)<0.05);
+    ind = find(rand(numel(g),1)<0.3);
     g(ind) = rand(length(ind),1);
 end
 %imshow(reshape(pinv(GM'*GM)*GM'*g,dim))
@@ -46,20 +44,20 @@ tic
 [L, S, obj, err, iter] = tcpcp(dim,g,GM,GM2,lambda,opts);
 toc
 
-maxP = max(abs(X(:)));
+maxP = max(abs(Xn(:)));
 
 figure(1)
 subplot(2,2,1)
-imshow(X/maxP)              % original picture
+imshow(X/maxP)% original picture
 title('Original Image');
 subplot(2,2,2)
-imshow(Xn/max(abs(X(:))))  % original picture
+imshow(Xn/maxP)% original picture
 title('Noise Observation');
 subplot(2,2,3)
-imshow(L/max(abs(X(:))))    % L solved by TCPCP
+imshow(L/maxP)% L solved by TCPCP
 title('L');
 subplot(2,2,4)
-imshow(S/max(abs(X(:))))    % S solved by TCPCP
+imshow(S/maxP)% S solved by TCPCP
 title('S');
 
 savefig('TCPCPresult.fig')
--- tcpcp.m
+++ tcpcp.m
@@ -1,4 +1,4 @@
-function [P,Q,obj,err,iter] = tcpcp(dim,g,GM,GM2,lambda,opts)
+function [L,S,obj,err,iter] = tcpcp(dim,g,GM,GM2,lambda,opts)
 
 % Tensor Compressive Principal Component Pursuit Algomrith
 %
@@ -29,13 +29,13 @@ function [P,Q,obj,err,iter] = tcpcp(dim,g,GM,GM2,lambda,opts)
 %       err     -    residual 
 %       iter    -    number of iterations
 
-tol = 1e-8; 
+tol = 1e-9; 
 max_iter = 500;
 rho = 1.05;
-mu = 1e-1;
+mu = 0.01;
 max_mu = 1e10;
 DEBUG = 0;
-penalty = 10;
+penalty = 0.01;
 
 if ~exist('opts', 'var')
     opts = [];
@@ -64,15 +64,15 @@ for iter = 1 : max_iter
     Lk = L;
     Sk = S;
     % update P
-    [P,tnnP] = prox_tnn(L+Z1/mu,1/mu);
+    [P,tnnP] = prox_tnn(X-S+Z1/mu,1/mu);
     % update Q
-    Q = prox_l1(S+Z2/mu,lambda/mu);
-    penalty = mu;
+    Q = prox_l1(X-L+Z2/mu,lambda/mu);
+    %penalty = mu;
     % update L
-    [ll,~] = cgs((penalty*GM2+mu*eye(m)),(penalty*GM'*g+mu*P(:)-Z1(:)-penalty*GM2*S(:)),1e-8,300);
+    [ll,~] = cgs((penalty*GM2+mu*eye(m)),(penalty*GM'*g+mu*P(:)-Z1(:)-penalty*GM2*S(:)),1e-8,200);
     L = reshape(ll,dim);
     % update S
-    [ss,~] = cgs((penalty*GM2+mu*eye(m)),(penalty*GM'*g+mu*Q(:)-Z2(:)-penalty*GM2*L(:)),1e-8,300);
+    [ss,~] = cgs((penalty*GM2+mu*eye(m)),(penalty*GM'*g+mu*Q(:)-Z2(:)-penalty*GM2*L(:)),1e-8,200);
     S = reshape(ss,dim);
     % dual update difference
     dZ1 = L-P;
@@ -83,7 +83,7 @@ for iter = 1 : max_iter
     if DEBUG
         if iter == 1 || mod(iter, 10) == 0
             obj = tnnP+lambda*norm(S(:),1);
-            err = norm(dZ1(:))+norm(dZ2(:))+norm(GM*(L(:)+S(:))-g);
+            err = norm(dZ1(:))+norm(dZ2(:))+norm(GM*(L(:)-S(:))-g);
             disp(['iter ' num2str(iter) ', mu=' num2str(mu) ...
                     ', obj=' num2str(obj) ', err=' num2str(err)...
                     ', norm(Z1)=' num2str(norm(abs(Z1(:)))) ', norm(Z2)=' num2str(norm(abs(Z2(:))))...
@@ -99,7 +99,7 @@ for iter = 1 : max_iter
     Z1 = Z1 + mu*dZ1;
     % dual update Z2
     Z2 = Z2 + mu*dZ2;
-    %X = L+S;
+     X = L+S;
     mu = min(rho*mu,max_mu);    
 end
 obj = tnnP+lambda*norm(S(:),1);
